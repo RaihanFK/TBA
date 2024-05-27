d6af46a725a2b2bc954158eea07c112f00ef7363
youkwhd <lolywk@tutanota.com>
2024-05-27 19:16:20 +0700

    feat: automatically add html tags

--- dfa.py
+++ dfa.py
@@ -1,32 +1,27 @@
-table: list[dict[str, int]] = [
-    {
-        '0': 1,
-        '1': 1,
-        '2': 1,
-        '3': 1,
-        '4': 1,
-        '5': 1,
-        '6': 1,
-        '7': 1,
-        '8': 1,
-        '9': 1,
-    },
-    {
-        '0': 1,
-        '1': 1,
-        '2': 1,
-        '3': 1,
-        '4': 1,
-        '5': 1,
-        '6': 1,
-        '7': 1,
-        '8': 1,
-        '9': 1,
-    }
-]
+table: list[dict[str, int]] = [{}, {}, {}]
 
 initial_state = 0
-final_states = [1]
+final_states = []
+
+table[0]['<'] = 2
+table[1]['/'] = 2
+
+def add_tag(name: str) -> None:
+    assert len(name) > 0
+    state = 2
+
+    for i in range(len(name)):
+        if name[i] in table[state]:
+            state = table[state][name[i]]
+            continue
+        
+        table[state][name[i]] = len(table)
+        state = len(table)
+        table.append({})
+
+    table[state]['>'] = len(table)
+    table.append({})
+    final_states.append(len(table) - 1)
 
 def matches(string: str) -> bool:
     state = initial_state
@@ -38,3 +33,37 @@ def matches(string: str) -> bool:
         state = table[state][ch]
 
     return state in final_states
+
+TAG = add_tag
+TAG("html")
+TAG("html5")
+TAG("html35")
+TAG("h1")
+TAG("p")
+TAG("body")
+TAG("title")
+
+assert not matches("")
+assert not matches("<")
+assert not matches(">")
+assert not matches("<h>")
+
+assert matches("<html>")
+assert not matches("<h1tml>")
+
+assert matches("<html5>")
+assert not matches("html5")
+
+assert matches("<html35>")
+assert not matches("html35>html")
+
+assert matches("<h1>")
+assert not matches("<h1")
+assert not matches("h1>")
+assert not matches("<h1>tml>")
+
+assert matches("<p>")
+assert not matches("<hp>")
+
+assert matches("<body>")
+assert matches("<title>")
